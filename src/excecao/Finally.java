651b25c1f99fcc54330c13e8d12306301394b1a6
rafadevgg <rafaelpc5671@gmail.com>
2026-08-17 18:54:29 -0300

    feat: Criação das classes finais Exceptions checadas e não checadas, finally e exception causadora

---
+++ src/excecao/Finally.java
@@ -0,0 +1,29 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Finally {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println(7 / sc.nextInt());
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Finally #01...");
+        }
+
+        try {
+            System.out.println(7 / sc.nextInt());
+        } finally {
+            System.out.println("Finally #02...");
+            sc.close();
+        }
+
+        System.out.println("Fim :)");
+
+    }
+
+}
---
+++ src/excecao/personalizadaB/NumeroForaIntervaloException.java
@@ -0,0 +1,15 @@
+package excecao.personalizadaB;
+
+public class NumeroForaIntervaloException extends Exception {
+
+    private String nomeDoAtributo;
+
+    public NumeroForaIntervaloException(String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage() {
+        return String.format("O número '%s' está fora do intervalo", nomeDoAtributo);
+    }
+
+}
